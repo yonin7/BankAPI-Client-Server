9b474d1dd8fe62d6150fd1bf5a03ecdbe3b6c1a5
yonin7 <yonin789@walla.com>
2022-01-22 17:48:43 +0200

    run build path

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+const publicPath = path.join(__dirname, 'client/build');
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(publicPath));
 
 app.get('/api/users', (req, res) => {
   try {
